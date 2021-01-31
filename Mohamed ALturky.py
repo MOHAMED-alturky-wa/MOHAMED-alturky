636dc732f3cbabef4411b601c4baf64c837e2156
MOHAMED-alturky-wa <78301628+MOHAMED-alturky-wa@users.noreply.github.com>
2021-01-31 14:13:46 +0200

    Add files via upload

---
+++ Mohamed ALturky.py
@@ -0,0 +1,295 @@
+import os
+os . system ("clear")
+import time,sys
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.10)
+		
+print ("\033[1;32mMohamed ALturky")
+jalan("Welcome to mt channel Mohamed ALturky WA linke whatsApp 01126796207")
+
+import os
+os . system ("clear")
+
+
+print ("\033[1;35mMohamed ALturky ALkeber FOoo")
+h = """ ▄▀▄ █░░ ▀█▀ █░█ █▀▀▄ █░▄▀ ▀▄░▄▀
+ █▀█ █░▄ ░█░ █░█ █▐█▀ █▀▄░ ░░█░░
+ ▀░▀ ▀▀▀ ░▀░ ░▀░ ▀░▀▀ ▀░▀▀ ░░▀░░
+ 
+____▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄_
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█--------█Mohamed ALturky
+█--------█Hack Facebook
+█----                  -█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"""
+print (h)
+password = input ("Type passward pleas: ")
+
+print ("\033[1;32mALturky WA")
+
+hh = input(">Type pleas< 1: ")
+
+gg = input(">ip accunt Facebook<: ")
+
+vv = input("Haw miny atacke hack accunt Facebook: ")
+import time
+
+print (" wate wate")
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+
+time.sleep(1)
+print ("hack Facebook wate")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook wate")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook wate")
+
+time.sleep(1)
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook wate")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook wate")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook wate")
+
+time.sleep(1)
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook wate")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook wate")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook wate")
+
+time.sleep(1)
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(0)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(1)
+print ("hack Facebook")
+
+time.sleep(11)
+print ("pleas wate mante")
+
+g = ("password Facebook 1234512345@@@")
+print (g)
